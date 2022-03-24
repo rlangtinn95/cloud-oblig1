60778fb448ea5300452721b98a434120205e0943
OlaHulleberg <ola.hulleberg@gmail.com>
2022-03-24 12:40:15 +0100

    NPM init

--- Oblig-1/script.js
+++ Oblig-1/script.js
@@ -1,14 +1,9 @@
 const http = require("http");
 const host = 'localhost';
-const port = 8000;
 
-const requestListener = function(req, res){
-    res.writehead(200);
-    res.end("my first server");
+function handleRequest(request, response) {
+    response.status(200);
+    response.send("Yooo");
 }
 
-const server = http.createServer(requestListener);
-
-server.listen(port, host() => {
-    console.log(`Server is running on http://${host}, ${port}`)
-});
+const server = http.createServer(handleRequest);
