79bf902cf8008676d0427826d35d3b0d3039bb10
OlaHulleberg <ola.hulleberg@gmail.com>
2022-03-24 12:49:33 +0100

    concept webserver

--- Oblig-1/server.js
+++ Oblig-1/server.js
@@ -1,9 +1,11 @@
 const http = require("http");
 const host = 'localhost';
+const port = 80;
 
 function handleRequest(request, response) {
-    response.status(200);
-    response.send("Yooo");
+    response.writeHead(200);
+    response.end("Yooo");
 }
 
 const server = http.createServer(handleRequest);
+server.listen(port);
