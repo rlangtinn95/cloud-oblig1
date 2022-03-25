4499bc913199437c5c52aaecada3ddc910ca1e3f
OlaHulleberg <ola.hulleberg@gmail.com>
2022-03-25 16:13:01 +0100

    Server latency

--- API/user.js
+++ API/user.js
@@ -1,19 +1,24 @@
 const userModel = require('../schema/userModel');
+const serverTimestamp = require('../timestamp');
 
 function handleCreate(req, res) {
+    var receivedTimestamp = Date.now();
     var query = req.body;
     var user = new userModel(query);
 
     user.save()
-    .then(() => {res.send({user: user})})
+    .then(() => {
+        res.send({user: user, timestamps: serverTimestamp(receivedTimestamp)})
+    })
     .catch((error) => {res.status(500).send({error: error})})
 }
 
 function handleRead(req, res) {
+    var receivedTimestamp = Date.now();
     var query = req.query;
     userModel.find(query).then((user) => {
         try {
-            res.send({user: user});
+            res.send({user: user, timestamps: serverTimestamp(receivedTimestamp)});
         } catch(error) {
             res.status(500).send({error: error});
         }
@@ -21,6 +26,7 @@ function handleRead(req, res) {
 }
 
 function handleUpdate(req, res) {
+    var receivedTimestamp = Date.now();
     var query = req.body;
     var filter = query.filter;
     delete query.filter;
@@ -28,11 +34,12 @@ function handleUpdate(req, res) {
         if(error)
             res.status(500).send({error: error});
         else
-            res.send({user: user});
+            res.send({user: user, timestamps: serverTimestamp(receivedTimestamp)});
     });
 }
 
 function handleDelete(req, res) {
+    var receivedTimestamp = Date.now();
     var query = req.body;
     var filter = query.filter;
     delete query.filter;
@@ -40,7 +47,7 @@ function handleDelete(req, res) {
         if(error)
             res.status(500).send({error: error});
         else
-            res.send({user: "deleted"});
+            res.send({user: "deleted", timestamps: serverTimestamp(receivedTimestamp)});
     });
 }
 
