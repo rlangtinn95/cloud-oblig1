548b55a3c1a00850326eecd44dac56f93b668ced
OlaHulleberg <ola.hulleberg@gmail.com>
2022-03-25 14:29:58 +0100

    Database connection

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
+const DBI = require('./db');
 const port = process.env.PORT || 80
 
 app.use(bodyParser.urlencoded({extended: true}));
 var jsonParser = bodyParser.json()
+
+DBI.initConnection();
 
 // API Endpoints
 require('./API/user')(app, jsonParser);
